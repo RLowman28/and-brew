b24ecbfd6f1f11091341153527386c96f411e732
Robbie Lowman <Robbie28@vt.edu>
2019-06-13 11:27:28 -0400

    Stripped the default content off of the generated source files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
+// Import React and the primary components for this app.
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+// Return a plain old h1 element until the components are tested.
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hang tight, something's coming.</h1>;
 }
 
+// Export the App component as the default for use in index.tsx.
 export default App;
